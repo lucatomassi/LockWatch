3d4d2c3d62c18a18148245cc6bfb5b8645f89259
Luca Tomassi <luca@tomassi.ca>
2021-03-13 21:45:00 -0500

    Application Skeleton -

---
+++ app/src/main/java/com/example/lockwatch/StatusActivity.java
@@ -0,0 +1,14 @@
+package com.example.lockwatch;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class StatusActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_status);
+    }
+}
